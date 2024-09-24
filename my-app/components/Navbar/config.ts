d912cf1e105d43b65487d0c5fa73a17bc429e0bc
denggf-xcurrency <154774209+denggf-xcurrency@users.noreply.github.com>
2024-09-24 22:54:15 +0800

    chore: defalut menu

--- my-app/components/Navbar/config.ts
+++ my-app/components/Navbar/config.ts
@@ -2,13 +2,16 @@ export const navs = [
   {
     label: '首页',
     value: '/',
+    key: 'home'
   },
   {
     label: '咨询',
     value: '/info',
+    key:'info'
   },
   {
     label: '标签',
-    value: '/tag',
+    value: '/tags',
+    key:'tags'
   },
 ];
--- my-app/pages/404.tsx
+++ my-app/pages/404.tsx
@@ -1,3 +1,5 @@
+import Layout from '@/components/Layout';
+
 export default function Custom404() {
   return (
     <div

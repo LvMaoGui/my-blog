efc6c59695bc8403b3d1055e8a7a7ab42f0e5c5f
denggf-xcurrency <154774209+denggf-xcurrency@users.noreply.github.com>
2024-09-24 22:55:14 +0800

    Revert "chore: defalut menu"
    This reverts commit d912cf1e105d43b65487d0c5fa73a17bc429e0bc.

--- my-app/components/Navbar/config.ts
+++ my-app/components/Navbar/config.ts
@@ -2,16 +2,13 @@ export const navs = [
   {
     label: '首页',
     value: '/',
-    key: 'home'
   },
   {
     label: '咨询',
     value: '/info',
-    key:'info'
   },
   {
     label: '标签',
-    value: '/tags',
-    key:'tags'
+    value: '/tag',
   },
 ];
--- my-app/pages/404.tsx
+++ my-app/pages/404.tsx
@@ -1,5 +1,3 @@
-import Layout from '@/components/Layout';
-
 export default function Custom404() {
   return (
     <div
